a2ad98a0942b748d20ec1603988a959917a51888
giorgi alavidze <alavidzegiorgi509@gmail.com>
2025-05-05 20:22:47 +0400

    commit

---
+++ day 99/classwork/script.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5];
+const sum = numbers.reduce((smt, smt2) => {
+    return smt + smt2;
+}, 2);
+
+const numbers1 = [1, 2, 3, 4, 5];
+const namravli = numbers.reduce((smting, smting2) => {
+    return smting * smting2;
+}, 4);
+
